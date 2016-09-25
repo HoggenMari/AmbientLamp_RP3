027bc6b3746aef002ab528e58ff56c27bef8793f
HoggenMari <m.hoggenmueller@gmail.com>
2016-09-25 16:41:20 +1000

    framerate 30

--- src/Sketch/Main.java
+++ src/Sketch/Main.java
@@ -192,7 +192,7 @@ public class Main extends PApplet implements SensorListener, VisualListener, Gen
 	
 	public void draw() {
 		
-		//frameRate(30);
+		frameRate(30);
 		//System.out.println("GENIUS: "+geniusMode);
 		if(frameCount%200==0){
 			//System.out.println("active:"+active+" next:"+next+" geniusCtr:"+geniusCtr);
